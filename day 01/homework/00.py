ffe64453600e06328aa97a0e78aa65a0e1437e47
xachiperadze <xachiperadevasili@gmail.com>
2024-05-10 20:25:05 +0400

    first commit

---
+++ day 01/homework/00.py
@@ -0,0 +1,68 @@
+name =  "vako"
+#name არის ცვლადი
+# = არის ცვლადისთვის მინიჭებული სიმბოლო
+# "vako" არის ცვლადისთვის მნიშვნელობა
+
+surname = "xachiperadze"
+
+# prin (name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+
+name =  "vako"  # ეს არის str (string) ტიპის ცვლადი
+
+
+age = 12 #ეს არის int (integer) მთელი რიცხვი
+
+height = 164.2  # ეს არის float ტიპის ცვლადი (ათწილადი)
+
+
+
+knows_programming =   True  #true ან False
+is_ugly = False    #snakecose(უბრალოდ წერის სტილი სტანდარტულად)   
+
+isugly = False #ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + surname)
+
+
+knows_programming =   True  #true ან False
+is_ugly = False    #snakecose(უბრალოდ წერის სტილი სტანდარტულად)   
+
+isugly = False #ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + surname)
+
+
+name = "vako"
+surname = "khaciperadze"
+age = "12"
+height = 164.2
+
+print(name + " " + surname + age + height) 
+
+
