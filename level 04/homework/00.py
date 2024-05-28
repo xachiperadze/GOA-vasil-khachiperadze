96aeefcaf1958ff80e14822bd1627ac346c8cc22
xachiperadze <xachiperadevasili@gmail.com>
2024-05-28 20:52:08 +0400

    commit

---
+++ level 04/homework/00.py
@@ -0,0 +1,16 @@
+print("name")
+print("vako")
+print("surname")
+print("xachiperadze")
+print("hobby")
+print("goashi swavla")
+
+
+# print(20 + 20) #მიმატება
+# print(300 - 20) #გამოკლება
+# print(100 * 100) #გამრავლება
+# print(800 // 2) #გაყოფა
+# print(25 ** 25) 
+    
+# texst-ტექსტი=string-სტრინგი
+# number-რუცხვი=integer-ინტეჯერი
